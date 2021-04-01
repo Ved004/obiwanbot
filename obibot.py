bac43562b124f2d6f5c04017a194431f6c1b6c62
RavenEbonscale <createhavoc@hotmail.com>
2021-03-31 21:05:06 -0400

    added random element

--- obibot.py
+++ obibot.py
@@ -1,8 +1,14 @@
-import praw ,configparser,os
+import praw ,configparser
+from random import choice
 config = configparser.ConfigParser()
 
 config.read('config.ini')
+list_quotes =[]
+f = open('qoutes.txt')
 
+for line in f:
+    list_quotes.append(line)
+print(choice(list_quotes))
 reddit = praw.Reddit(
     username=config['Api_keys']['username'],
     password=config['Api_keys']['password'],
@@ -10,6 +16,9 @@ reddit = praw.Reddit(
     client_secret =config['Api_keys']['secret'],
     user_agent = 'Repost bot by u/Purple_scale_boi'
 )
+
+
+
 reddit.validate_on_submit =True
 sub = reddit.subreddit(config['msc']['subreddit'])
 bot =reddit.user.me().name
@@ -19,7 +28,7 @@ def main(bot):
 
     for comment in sub.stream.comments():
         if comment.author != bot:
-            comment.reply('Hello There')
+            comment.reply(str(choice(list_quotes))) # you can change this to what ever you want
             print(f'commented on {comment.body}')
     
 main(bot)
